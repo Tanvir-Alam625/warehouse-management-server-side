885afe0a2ce19886ac088e8cf2e7f891213e6bbb
tanveer <mdtanviralamht731@gmail.com>
2022-04-29 16:17:14 +0600

    poject initialized

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("server running");
+});
+
+app.listen(port, () => {
+  console.log("server port is", port);
+});
